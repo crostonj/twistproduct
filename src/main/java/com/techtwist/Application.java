c44ebaa133f7dcbe84ab384aae32ef4a3d9d45d7
crostonj <crostonj@gmail.com>
2025-05-23 04:46:45 +0000

    Update MongoDB environment variable handling and add dotenv dependency

--- src/main/java/com/techtwist/Application.java
+++ src/main/java/com/techtwist/Application.java
@@ -2,11 +2,21 @@ package com.techtwist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        Dotenv dotenv = Dotenv.load();
+
+                // Set env vars for Spring to resolve ${...} in application.yaml
+        System.setProperty("MONGO_USERNAME", dotenv.get("MONGO_USERNAME"));
+        System.setProperty("MONGO_PASSWORD", dotenv.get("MONGO_PASSWORD"));
+        System.setProperty("MONGO_HOST", dotenv.get("MONGO_HOST"));
+        System.setProperty("MONGO_PORT", dotenv.get("MONGO_PORT"));
+
+        
+        SpringApplication.run(Application.class, args);
     }
 }

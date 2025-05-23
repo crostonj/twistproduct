6216cc19c46a7aa949b124ee93ac53a856728fcf
crostonj <crostonj@gmail.com>
2025-05-23 21:25:30 +0000

    Remove commented-out MongoDB environment variable settings in Application.java

--- src/main/java/com/techtwist/Application.java
+++ src/main/java/com/techtwist/Application.java
@@ -11,10 +11,10 @@ public class Application {
         Dotenv dotenv = Dotenv.load();
 
                 // Set env vars for Spring to resolve ${...} in application.yaml
-        System.setProperty("MONGO_USERNAME", dotenv.get("MONGO_USERNAME"));
-        System.setProperty("MONGO_PASSWORD", dotenv.get("MONGO_PASSWORD"));
-        System.setProperty("MONGO_HOST", dotenv.get("MONGO_HOST"));
-        System.setProperty("MONGO_PORT", dotenv.get("MONGO_PORT"));
+       // System.setProperty("MONGO_USERNAME", dotenv.get("MONGO_USERNAME"));
+       // System.setProperty("MONGO_PASSWORD", dotenv.get("MONGO_PASSWORD"));
+       // System.setProperty("MONGO_HOST", dotenv.get("MONGO_HOST"));
+       // System.setProperty("MONGO_PORT", dotenv.get("MONGO_PORT"));
 
         
         SpringApplication.run(Application.class, args);

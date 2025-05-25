74f5b02217f1727c2a21ce3aeedecc960ed827e6
crostonj <crostonj@gmail.com>
2025-05-25 15:10:09 +0000

    Remove commented-out environment variable settings in Application.java

--- src/main/java/com/techtwist/Application.java
+++ src/main/java/com/techtwist/Application.java
@@ -7,16 +7,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-
-                // Set env vars for Spring to resolve ${...} in application.yaml
-       // System.setProperty("MONGO_USERNAME", dotenv.get("MONGO_USERNAME"));
-       // System.setProperty("MONGO_PASSWORD", dotenv.get("MONGO_PASSWORD"));
-       // System.setProperty("MONGO_HOST", dotenv.get("MONGO_HOST"));
-       // System.setProperty("MONGO_PORT", dotenv.get("MONGO_PORT"));
-
-        
+    public static void main(String[] args) {  
         SpringApplication.run(Application.class, args);
     }
 }

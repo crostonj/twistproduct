43bce8ffdd118b735fdbed9a192c6b82e7c30199
crostonj <crostonj@gmail.com>
2025-05-28 04:41:48 +0000

    Add debug logging for MongoDB environment variables in Application.java

--- src/main/java/com/techtwist/Application.java
+++ src/main/java/com/techtwist/Application.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+    static {
+        System.out.println("DEBUG: MONGO_HOST=" + System.getenv("MONGO_HOST"));
+        System.out.println("DEBUG: MONGO_PORT=" + System.getenv("MONGO_PORT"));
+        System.out.println("DEBUG: MONGO_USERNAME=" + System.getenv("MONGO_USERNAME"));
+    }
     public static void main(String[] args) {  
         SpringApplication.run(Application.class, args);
     }

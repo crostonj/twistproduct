13802028cf5bcdab10570f05181391f5fa5cd586
crostonj <crostonj@gmail.com>
2025-05-28 04:59:36 +0000

    Add debug logging for MongoDB password environment variable in Application.java

--- src/main/java/com/techtwist/Application.java
+++ src/main/java/com/techtwist/Application.java
@@ -10,6 +10,8 @@ public class Application {
         System.out.println("DEBUG: MONGO_HOST=" + System.getenv("MONGO_HOST"));
         System.out.println("DEBUG: MONGO_PORT=" + System.getenv("MONGO_PORT"));
         System.out.println("DEBUG: MONGO_USERNAME=" + System.getenv("MONGO_USERNAME"));
+        System.out.println("DEBUG: MONGO_PASSWORD_IS_SET=" + (System.getenv("MONGO_PASSWORD") != null && !System.getenv("MONGO_PASSWORD").isEmpty())); // Don't print the actual password
+        System.out.println("DEBUG:  MONGO_PASSWORD=" + System.getenv(" MONGO_PASSWORD"));
     }
     public static void main(String[] args) {  
         SpringApplication.run(Application.class, args);
